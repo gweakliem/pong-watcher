7d85a289f79e1e89848572d431d027aa83711f00
Gordon Weakliem <gordon.weakliem@gmail.com>
2016-10-05 18:33:31 +0000

    Integrated gpizero interface

--- prototype/mqtt_publish.py
+++ prototype/mqtt_publish.py
@@ -1,21 +1,35 @@
 import paho.mqtt.client as paho
 import time
 import datetime
-import random
-#from gpiozero import MotionSensor
+#import random
+from gpiozero import MotionSensor
 
 def read_from_sensor():
     return random.getrandbits(1)
 
 def on_publish(client, userdata, mid):
-    print("mid: " + str(mid))
+    print("mid: " + str(mid) + " " + str(userdata))
 
+pir = MotionSensor(4)
 client = paho.Client()
 client.on_publish = on_publish
 client.connect("localhost", 1883)
 client.loop_start()
 
 while True:
-    motion_detected = read_from_sensor()
-    (rc, mid) = client.publish("motion_sensor/events", str((datetime.datetime.utcnow().isoformat(), motion_detected)), qos=1)
-    time.sleep(3)
+    pir.wait_for_motion()   
+    #motion_detected = read_from_sensor()
+    (rc, mid) = client.publish("motion_sensor/events",
+                               str((datetime.datetime.utcnow().isoformat(),
+                                    1)), qos=1)
+    while pir.motion_detected:
+	time.sleep(1)
+	client.publish("motion_sensor/events",
+                       str((datetime.datetime.utcnow().isoformat(),
+                            1)), qos=1)
+    client.publish("motion_sensor/events",
+                   str((datetime.datetime.utcnow().isoformat(),
+                        0)), qos=1)
+
+print("Shutting down")
+    
