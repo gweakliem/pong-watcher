fe9ecaa40b3e44ff9f7c07d242b29e566dfd2637
Gordon Weakliem <gordon.weakliem@gmail.com>
2016-11-29 16:45:28 -0700

    Updated motion.py to use template from mqtt_client

--- prototype/motion.py
+++ prototype/motion.py
@@ -1,23 +1,33 @@
+import paho.mqtt.client as paho
+import time
+import datetime
+import random
+import json
 from gpiozero import MotionSensor
 
-import logging
-import time
-from datetime import datetime
+def read_from_sensor():
+	pir.wait_for_motion()
 
-logfile = '/home/pi/nb-' + str(datetime.now().strftime("%Y%m%d-%H%M")) +".csv"
-logging.basicConfig(filename=logfile, level=logging.DEBUG,
-	format='%(asctime)s %(message)s',
-datefmt='%Y-%m-%d, %H:%M:%S,')
+def on_publish(client, userdata, mid):
+    print("mid: " + userdata)
 
 pir = MotionSensor(4)
+client = paho.Client()
+client.on_publish = on_publish
+client.connect("localhost", 1883)
+client.loop_start()
 
-print('Starting')
-logging.info('Starting')
 while True:
-	pir.wait_for_motion()
-	logging.info('Motion detected')
+    motion_detected = read_from_sensor()
+    (rc, mid) = client.publish("motion_sensor/events", json.dumps({'ts': datetime.datetime.utcnow().isoformat(), 'm': motion_detected}), qos=1)
 	while pir.motion_detected:
-		time.sleep(1)
-		logging.info('Motion detected')
-	logging.info('Motion Ended')
+		time.sleep(10)
+        (rc, mid) = client.publish("motion_sensor/events", json.dumps({'ts': datetime.datetime.utcnow().isoformat(), 'm': motion_detected}), qos=1)
+    (rc, mid) = client.publish("motion_sensor/events", json.dumps({'ts': datetime.datetime.utcnow().isoformat(), 'm': motion_detected}), qos=1)
+
+#logfile = '/home/pi/nb-' + str(datetime.now().strftime("%Y%m%d-%H%M")) +".csv"
+#logging.basicConfig(filename=logfile, level=logging.DEBUG,
+#	format='%(asctime)s %(message)s',
+#datefmt='%Y-%m-%d, %H:%M:%S,')
+
 
